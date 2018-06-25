4d60b08b88659503e12fff0bf992bc2f098dec22
Thomas Dodson <tw.dodson@yahoo.co.uk>
2018-06-25 22:56:46 +0000

    initial repo

---
+++ spec/calcSpec.js
@@ -0,0 +1,37 @@
+
+describe("fizzBuzz", function() {
+    // A test suite begins with a call to the global Jasmine function describe with two parameters: a string and a function.
+    
+    beforeEach(function() {
+        fizzbuzz = new fizzBuzz();
+    });
+    
+    describe("Returns number, fizz, buzz or fizzbuzz", function() {
+        // Specs are defined by calling the global Jasmine function it
+        it("should exist", function() {
+            // Expectations are built with the function expect which takes a value, called the actual. 
+            // Each matcher implements a boolean comparison between the actual value and the expected value.
+            // Any matcher can evaluate to a negative assertion by chaining the call to expect with a not before calling the matcher.
+            expect(fizzBuzz).toBeDefined();
+        });
+        
+        it("should return Fizz when called as fizzBuzz(9)", function() {
+            var result = fizzBuzz(9)
+            expect(result).toBeDefined("Fizz");
+        });
+                
+        it("should return Buzz when called as fizzBuzz(10)", function() {
+            var result = fizzBuzz(10)
+            expect(result).toBeDefined("Buzz");
+        });
+        
+        it("should return FizzBuzz when called as fizzBuzz(15)", function() {
+            var result = fizzBuzz(15)
+            expect(result).toBeDefined("FizzBuzz");
+        });
+        it("should return 2 when called as fizzBuzz(2)", function() {
+            var result = fizzBuzz(2)
+            expect(result).toBeDefined(2);
+        });
+    });
+});
